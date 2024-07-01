ee47f056e1bb87b5a93098d7e7336aeee844197d
Jabed <jabedalimollah7@gmail.com>
2024-07-01 15:50:45 +0530

    check duplicate username and error

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -4,9 +4,20 @@ import { User } from "../models/user.model.js";
 const signup = async (req, res) => {
   try {
     const data = req.body;
-    const newUser = new User(data);
-    const response = await newUser.save();
-    res.status(200).json(response);
+    const checkUserName = await User.findOne({ username: data.username });
+    // console.log(checkUserName);
+    const checkEmail = await User.findOne({ email: data.email });
+    if (checkUserName) {
+      // console.log("username already exits");
+      res.status(400).json({ message: "username already exits" });
+    } else if (checkEmail) {
+      // console.log("email already exits");
+      res.status(400).json({ message: "email already exits" });
+    } else {
+      const newUser = new User(data);
+      const response = await newUser.save();
+      res.status(200).json(response);
+    }
   } catch (err) {
     res.status(500).json({ error: "Sign Up problem" });
     // console.log("sign up problem", err);
